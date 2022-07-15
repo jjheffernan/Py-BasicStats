635091acdf1513d84784a548ef8f75c84c91661d
jjheffernan <jjheffernan99@gmail.com>
2022-07-15 12:04:22 -0400

    added count mean, and median functions. Working on tests.

--- statzcw/z.py
+++ statzcw/z.py
@@ -1,31 +1,53 @@
 """
-
+    Test python module. This will be deleted when re-packaging the below functions.
+    Functions will live in similarly named files within statscw package,
+        under different name to avoid shodowing outer scope.
+    Made by jjheffernan on week 4 of Zip Code Wilmington
 """
-import
+
+avg_val = 0
+
 
 def count(funlist: list) -> float:
     return len(funlist)
     # pass # escape pass
 
 
-def mean(funList: List[]) -> float:
-
+def mean(funlist: list) -> float:
+    global avg_val
+    average = sum(funlist)/len(funlist)
+    avg_val = int(average)
+    return average
     pass # escape pass
 
-def mode(funList: List[]) -> float:
+
+def mode(funlist: list) -> float:
+    # this might need lots of if statements
+    # min(), len(),
     pass
 
-def median(funList: List[]) -> float:
+
+def median(funlist: list) -> float:
+    funlist.sort()
+    if len(funlist) % 2 !=0:
+        return funlist[int((len(funlist)/2)-1)]
+    else:
+        return funlist[int((len(funlist)-1)/2)]
+
     pass
 
-def variance(funList: List[]) -> float:
+
+def variance(funlist: list) -> float:
     pass
 
-def stddev(funList: List[]) -> float:
+
+def stddev(funlist: list) -> float:
     pass
 
-def stderr(funList: List[]) -> float:
+
+def stderr(funlist: list) -> float:
     pass
 
-def corr(listx: List[], listy: List[]) -> float:
+
+def corr(listx: list, listy: list) -> float:
     pass
